346a1fc934a524953793cdbb26225df9e6b1969e
delchev <delchevn@gmail.com>
2016-04-30 11:30:16 -0400

    console API built-in

--- core_api/ScriptingServices/api/console.js
+++
@@ -1,6 +0,0 @@
-/* globals $ */
-/* eslint-env node, dirigible */
-
-exports.getConsole = function() {
-	return $.getSystemOutput();
-};
---
+++ core_api/TestCases/api/console_test.js
@@ -0,0 +1,8 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+console.info("Info message: %s", "Hello World!");
+console.error("Error message.");
+console.warn("Warning message.");
+console.log("Log message.");
+console.trace("Trace.");
