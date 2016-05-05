6acb44084ba23ee425d039cf57e7733542f32437
ThuF <ThuF@users.noreply.github.com>
2016-05-05 19:00:32 +0300

    [Test] API v2.0 - Session (Info)
    #5

---
+++ core_api/TestCases/api/session/session_info_test.js
@@ -0,0 +1,20 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var response = require('api/response');
+var session = require('api/session');
+
+var info = {
+	"id": session.getId(),
+	"creationTime": new Date(session.getCreationTime()),
+	"lastAccessedTime": session.getLastAccessedTime(),
+	"maxInactiveInterval": session.getMaxInactiveInterval()
+	
+};
+
+session.setMaxInactiveInterval(5000);
+info.newMaxInactiveInterval = session.getMaxInactiveInterval();
+
+response.println("[Info]: " + JSON.stringify(info));
+response.flush();
+response.close();
