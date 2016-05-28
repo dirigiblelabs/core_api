da831dadf0d60d522825fb3ac909bd81151235f1
delchev <delchevn@gmail.com>
2016-05-28 15:26:44 -0400

    env added

--- core_api/TestCases/api/globals_test.js
+++ core_api/TestCases/api/globals_test.js
@@ -1,8 +1,8 @@
 /* globals $ */
 /* eslint-env node, dirigible */
 
-var response = require('api/http/response');
 var globals = require('api/globals');
+var response = require('api/http/response');
 
 globals.set("attr1", "value1");
 var attr = globals.get("attr1");
