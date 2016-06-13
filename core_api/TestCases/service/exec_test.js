d1f2ab8ff5796e9dae2cb165df0ee77c2e7522c3
delchev <delchevn@gmail.com>
2016-06-13 23:21:47 +0300

    refactoring

--- core_api/TestCases/service/exec_test.js
+++ core_api/TestCases/service/exec_test.js
@@ -4,12 +4,14 @@
 var exec = require('service/exec');
 var response = require('net/http/response');
 
-// Test
-var result = exec.test("/service/exec_target_test.js", {"input_param": "input_param_value"});
+var context = {"input_param": "input_param_value"};
+
+// Test execution
+var result = exec.test("/service/exec_target_test.js", context);
 
 response.println("Printing output parameter: " + result.context.output_param);
 
-// use also exec.js, exec.flow, exec.job, exec.sql, exec.command ...
+// use also exec.js(), exec.flow(), exec.job(), exec.sql(), exec.command() ...
 
 response.flush();
 response.close();
