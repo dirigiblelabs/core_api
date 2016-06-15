d3e48d732fd72023a4f40b7915d4e60515576214
delchev <delchevn@gmail.com>
2016-06-15 11:40:29 +0300

    thread notify test added

---
+++ core_api/TestCases/thread/thread_notify_test.js
@@ -0,0 +1,47 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var thread = require('core/thread');
+var response = require('net/http/response');
+
+response.setContentType("text/plain; charset=UTF-8");
+response.setCharacterEncoding("UTF-8");
+
+var obj = { 
+	syncWait: thread.sync(function () { 
+    		console.log("I am synchronized waiter");
+    		this.wait(5000);
+    		console.log("I waited 2s or I was interrupted?");
+   	}),
+   	syncNotify: thread.sync(function () { 
+    		console.log("I am synchronized notifier");
+    		this.notifyAll();
+    		console.log("I notified all");
+   	})
+};
+
+function waitable() {
+	// 'syncWait' is a "synchronized" method.
+	obj.syncWait();
+};
+
+function notifyable() {
+	// 'syncWait' is a "synchronized" method.
+	obj.syncNotify();
+};
+
+var waiter = thread.create(waitable, "I am the waiter thread");
+console.log(waiter.getName());
+waiter.start();
+console.log("Waiter started.");
+//waiter.join(); // to be able to print to the response
+
+var notifier = thread.create(notifyable, "I am the notifier thread");
+console.log(notifier.getName());
+notifier.start();
+console.log("Notifier started.");
+
+response.println("See log...");
+
+response.flush();
+response.close();
---
+++ core_api/TestCases/thread/thread_wait_test.js
@@ -0,0 +1,30 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var thread = require('core/thread');
+var response = require('net/http/response');
+
+response.setContentType("text/plain; charset=UTF-8");
+response.setCharacterEncoding("UTF-8");
+
+var obj = { 
+	syncWait: thread.sync(function () { 
+    		response.println("I am synchronized waiter");
+    		this.wait(2000);
+    		response.println("I waited 2s");
+   	})
+};
+
+function waitable() {
+	// 'syncWait' is a "synchronized" method.
+	obj.syncWait();
+};
+
+var waiter = thread.create(waitable, "I am the waiter thread");
+response.println(waiter.getName());
+waiter.start();
+response.println("Waiter started.");
+waiter.join(); // to be able to print to the response
+
+response.flush();
+response.close();
