7444e8703a17552e4c10ba082c7866123204cce2
delchev <delchevn@gmail.com>
2016-10-19 21:55:24 +0300

    file and folder added to workspaces

--- core_api/ScriptingServices/platform/workspaces.js
+++ core_api/ScriptingServices/platform/workspaces.js
@@ -11,6 +11,8 @@
 /* globals $ java */
 /* eslint-env node, dirigible */
 
+var streams = require('io/streams');
+
 exports.getWorkspace = function() {
 	return new Workspace($.getWorkspacesService().getWorkspace($.getRequest()));
 };
@@ -44,6 +46,7 @@ function WorkspaceRoot(internalWorkspaceRoot) {
 	this.internalWorkspaceRoot = internalWorkspaceRoot;
 	this.getInternalObject = workspaceRootGetInternalObject;
 	this.getProjects = workspaceRootGetProjects;
+	this.getProject = workspaceRootGetProject;
 }
 
 function workspaceRootGetInternalObject() {
@@ -60,6 +63,11 @@ function workspaceRootGetProjects() {
 	return projects;
 }
 
+function workspaceRootGetProject(projectName) {
+	var internalProject = this.internalWorkspaceRoot.getProject(projectName);
+	return new Project(internalProject);
+}
+
 /**
  * Project object
  */
@@ -67,6 +75,12 @@ function Project(internalProject) {
 	this.internalProject = internalProject;
 	this.getInternalObject = projectGetInternalObject;
 	this.getName = projectGetName;
+	this.create = projectCreate;
+	this.delete = projectDelete;
+	this.open = projectOpen;
+	this.close = projectClose;
+	this.getFolder = projectGetFolder;
+	this.getFile = projectGetFile;
 }
 
 function projectGetInternalObject() {
@@ -75,4 +89,129 @@ function projectGetInternalObject() {
 
 function projectGetName() {
 	return this.internalProject.getName();
+}
+
+function projectCreate() {
+	return this.internalProject.create(null);
+}
+
+function projectDelete() {
+	return this.internalProject.delete(true, true, null);
+}
+
+function projectOpen() {
+	return this.internalProject.open(null);
+}
+
+function projectClose() {
+	return this.internalProject.close(null);
+}
+
+function projectGetFolder(name) {
+	var internalFolder = this.internalProject.getFolder(name);
+	return new Folder(internalFolder);
+}
+
+function projectGetFile(name) {
+	var internalFile = this.internalProject.getFile(name);
+	return new File(internalFile);
+}
+
+
+/**
+ * Folder object
+ */
+function Folder(internalFolder) {
+	this.internalFolder = internalFolder;
+	this.getInternalObject = folderGetInternalObject;
+	this.create = folderCreate;
+	this.delete = folderDelete;
+	this.getFolder = folderGetFolder;
+	this.getFile = folderGetFile;
+	this.getName = folderGetName;
+	this.getFullPath = folderGetFullPath;
+	this.exists = folderExists;
+}
+
+function folderGetInternalObject() {
+	return this.internalFolder;
+}
+
+function folderCreate() {
+	this.internalFolder.create(true, true, null);
+}
+
+function folderDelete() {
+	this.internalFolder.delete(true, false, null);
+}
+
+function folderGetFolder(name) {
+	var internalFolder = this.internalFolder.getFolder(name);
+	return new Folder(internalFolder);
+}
+
+function folderGetFile(name) {
+	var internalFile = this.internalFolder.getFile(name);
+	return new File(internalFile);
+}
+
+function folderGetName() {
+	return this.internalFolder.getName();
+}
+
+function folderGetFullPath() {
+	return this.internalFolder.getFullPath().toString();
+}
+
+function folderExists() {
+	return this.internalFolder.exists();
+}
+
+
+
+/**
+ * File object
+ */
+function File(internalFile) {
+	this.internalFile = internalFile;
+	this.getInternalObject = fileGetInternalObject;
+	this.create = fileCreate;
+	this.delete = fileDelete;
+	this.getContents = fileGetContents;
+	this.setContents = fileSetContents;
+	this.getName = fileGetName;
+	this.getFullPath = fileGetFullPath;
+	this.exists = fileExists;
+}
+
+function fileGetInternalObject() {
+	return this.internalFile;
+}
+
+function fileCreate(inputStream) {
+	this.internalFile.create(inputStream.getInternalObject());
+}
+
+function fileDelete() {
+	this.internalFile.delete(true, false, null);
+}
+
+function fileGetContents() {
+	return new streams.InputStream(this.internalFile.getContents());
+}
+
+function fileSetContents(inputStream) {
+	this.internalFile.setContents(inputStream.getInternalObject(), true, false, null);
+}
+
+function fileGetName() {
+	return this.internalFile.getName();
+}
+
+function fileGetFullPath() {
+	return this.internalFile.getFullPath().toString();
+}
+
+function fileExists() {
+	return this.internalFile.exists();
 }
