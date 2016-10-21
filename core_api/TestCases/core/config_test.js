6359df43c18843ecd2ba7c2a461988dc88a025e9
ThuF <ThuF@users.noreply.github.com>
2016-10-21 17:53:33 +0300

    Update config_test.js

--- core_api/TestCases/core/config_test.js
+++ core_api/TestCases/core/config_test.js
@@ -2,25 +2,43 @@
 /* eslint-env node, dirigible */
 
 var config = require('core/config');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-var properties = {
-		"property1": "value1",
-		"property2": "value2"
-	}
+config.set('/path/to/property', 'key1', 'value1');
+executeTests();
 
-config.set("/path/to/property", "key1", "value1");
-config.setJson("/path/to/properties", JSON.stringify(properties));
+function executeTests() {
+	var testResult = tests.execute([testSetProperty, testSetProperties]);
 
-response.println(config.get("/path/to/property", "key1"));
-var result = JSON.parse(config.getJson("/path/to/properties"));
-response.println(result.property2);
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
+}
 
-config.delete("/path/to/property");
-config.delete("/path/to/properties");
+function testSetProperty() {
+	var expectedPath = '/path/to/property';
+	var expectedKey = 'key1';
+	var expectedValue = 'value1';
 
-response.println(config.get("/path/to/property"));
-response.println(config.get("/path/to/properties"));
+	config.set(expectedPath, expectedKey, expectedValue);
 
-response.flush();
-response.close();
+	var actualValue = config.get(expectedPath, expectedKey);
+	assert.assertNotNull(actualValue, 'The actual property is null!');
+}
+
+function testSetProperties() {
+	var expectedPath = '/path/to/properties';
+	var expectedProperties = {
+		'property1': 'value1'
+	};
+
+	config.setJson(expectedPath, JSON.stringify(expectedProperties));
+
+	var actualProperties = config.getJson('/path/to/properties');
+	assert.assertNotNull(actualProperties, 'The actual property is null!');
+	assert.assertEquals(expectedProperties, JSON.parse(actualProperties), 'The expected properties doesn\'t match the actual properties!');
+}
