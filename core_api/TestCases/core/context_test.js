34481b5f1072ae2bc8f3f62165e6966cc62b2fe5
ThuF <jordan.p.pavlov@gmail.com>
2016-10-26 15:29:39 +0300

    Updates Context API Test

--- core_api/TestCases/core/context_test.js
+++ core_api/TestCases/core/context_test.js
@@ -2,11 +2,34 @@
 /* eslint-env node, dirigible */
 
 var context = require('core/context');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-context.set("attr1", "value1");
-var attr = context.get("attr1");
+executeTests();
 
-response.println("[Attribute]: " + attr);
-response.flush();
-response.close();
+function executeTests() {
+	var testResult = tests.execute([testGetContext, testSetContext]);
+
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
+}
+
+function testGetContext() {
+	var expectedContextAttribute = context.get('notDefinedAttribute');
+	assert.assertNull(expectedContextAttribute, 'The expected context attribute should be null!');
+}
+
+function testSetContext() {
+	var expectedContextAttributeName = 'testAttributeName';
+	var expectedContextAttributeValue = 'testAttributeValue';
+
+	context.set(expectedContextAttributeName, expectedContextAttributeValue);
+
+	var actualContextAttributeValue = context.get(expectedContextAttributeName);
+	assert.assertNotNull(actualContextAttributeValue, 'The expected context attribute value is null!');
+	assert.assertEquals(expectedContextAttributeValue, actualContextAttributeValue, 'The expected context attribute value does not match the actual context attribute value!');
+}
