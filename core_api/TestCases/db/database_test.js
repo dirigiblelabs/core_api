25d3a897ac7991c666a671f72f44ff543c9f37a1
ThuF <jordan.p.pavlov@gmail.com>
2016-10-26 15:41:54 +0300

    Updates Database API Test

--- core_api/TestCases/db/database_test.js
+++ core_api/TestCases/db/database_test.js
@@ -2,28 +2,47 @@
 /* eslint-env node, dirigible */
 
 var database = require('db/database');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-var datasource = database.getDatasource(); // default
-//var datasource = db.getNamedDatasource("name-of-the-datasource");
+executeTests();
 
-var connection = datasource.getConnection();
-try {
-    var statement = connection.prepareStatement("select * from DGB_FILES where FILE_PATH like ?");
-    var i = 0;
-    statement.setString(++i, "%");
-    var resultSet = statement.executeQuery();
-    while (resultSet.next()) {
-        response.println("[path]: " + resultSet.getString("FILE_PATH"));
-    }
-    resultSet.close();
-    statement.close();
-} catch(e) {
-	console.trace(e);
-    response.println(e.message);
-} finally {
-    connection.close();
+function executeTests() {
+	var testResult = tests.execute([testGetDatasource, testGetConnection, testExecuteQuery]);
+
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
 }
 
-response.flush();
-response.close();
+function testGetDatasource() {
+	var datasource = database.getDatasource();
+	//var datasource = db.getNamedDatasource("name-of-the-datasource");
+	assert.assertNotNull(datasource, 'Tha datasource is null!');
+}
+
+function testGetConnection() {
+	var datasource = database.getDatasource();
+	var connection = datasource.getConnection();
+	assert.assertNotNull(connection, 'The database connection is null!');
+}
+
+function testExecuteQuery() {
+	var datasource = database.getDatasource();
+	var connection = datasource.getConnection();
+	try {
+	    var statement = connection.prepareStatement("select * from DGB_FILES where FILE_PATH like ?");
+	    statement.setString(1, "%");
+	    var resultSet = statement.executeQuery();
+	    while (resultSet.next()) {
+	        assert.assertNotNull(resultSet.getString("FILE_PATH"), 'The FILE_PATH is null!');
+	    }
+	    resultSet.close();
+	    statement.close();
+	} finally {
+	    connection.close();
+	}
+}
