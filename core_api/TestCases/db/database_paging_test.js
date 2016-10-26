ee18db50e8310786c0fae16a99e04982e6e494af
ThuF <jordan.p.pavlov@gmail.com>
2016-10-26 16:27:14 +0300

    Updates Database API Test - Paging

--- core_api/TestCases/db/database_paging_test.js
+++ core_api/TestCases/db/database_paging_test.js
@@ -2,15 +2,29 @@
 /* eslint-env node, dirigible */
 
 var database = require('db/database');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-var datasource = database.getDatasource();
+executeTests();
 
-var paging = datasource.getPaging();
+function executeTests() {
+	var testResult = tests.execute([testPaging]);
 
-// only one of both below will return non-empty result - depends on the database dialect
-response.println(paging.genLimitAndOffset(100, 200));
-response.println(paging.genTopAndStart(300, 400));
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
+}
 
-response.flush();
-response.close();
+function testPaging() {
+	var datasource = database.getDatasource();
+	var paging = datasource.getPaging();
+
+	// only one of both below will return non-empty result - depends on the database dialect
+	var pagingQuery = paging.genLimitAndOffset(100, 200);
+	pagingQuery += paging.genTopAndStart(300, 400)
+
+	assert.assertNotNull(pagingQuery, 'The paging query is null!');
+}
