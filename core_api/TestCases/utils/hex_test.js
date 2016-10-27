9e527f0f4dfc7413d995cc847ca1325cefc4313b
ThuF <jordan.p.pavlov@gmail.com>
2016-10-27 15:19:53 +0300

    Updates Hex API Test

--- core_api/TestCases/utils/hex_test.js
+++ core_api/TestCases/utils/hex_test.js
@@ -2,10 +2,36 @@
 /* eslint-env node, dirigible */
 
 var hex = require('utils/hex');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-response.println(hex.encode('Hex Encoded'));
-response.println(hex.decode('48657820456e636f646564'));
+const TEST_PLAIN_TEXT = 'Hex Encoded';
+const TEST_ENCODED_TEXT = '48657820456e636f646564';
 
-response.flush();
-response.close();
+executeTests();
+
+function executeTests() {
+	var testResult = tests.execute([
+		testEncode,
+		testDecode
+	]);
+
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
+}
+
+function testEncode() {
+	var encoded = hex.encode(TEST_PLAIN_TEXT);
+	assert.assertNotNull(encoded);
+	assert.assertEquals(TEST_ENCODED_TEXT, encoded);
+}
+
+function testDecode() {
+	var decoded = hex.decode(TEST_ENCODED_TEXT);
+	assert.assertNotNull(decoded);
+	assert.assertEquals(TEST_PLAIN_TEXT, decoded);
+}
