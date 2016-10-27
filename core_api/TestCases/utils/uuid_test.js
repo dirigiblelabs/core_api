e0889ac9494c75c5c4d003986258817197f303ca
ThuF <jordan.p.pavlov@gmail.com>
2016-10-27 15:21:49 +0300

    Updates UUID API Test

--- core_api/TestCases/utils/uuid_test.js
+++ core_api/TestCases/utils/uuid_test.js
@@ -2,10 +2,29 @@
 /* eslint-env node, dirigible */
 
 var uuid = require('utils/uuid');
+
+var assert = require('core/assert');
+var tests = require('service/tests');
 var response = require('net/http/response');
 
-response.println(uuid.random());
-response.println(uuid.validate('14a3ddce-f86d-4f51-a2e0-6e497b94bbe5'));
+executeTests();
 
-response.flush();
-response.close();
+function executeTests() {
+	var testResult = tests.execute([
+		testRandom,
+		testValidate
+	]);
+
+	response.setStatus(tests.getHttpStatus(testResult));
+	response.println(tests.getText(testResult));
+	response.flush();
+	response.close();
+}
+
+function testRandom() {
+	assert.assertNotNull(uuid.random());
+}
+
+function testValidate() {
+	assert.assertTrue(uuid.validate('14a3ddce-f86d-4f51-a2e0-6e497b94bbe5'));
+}
