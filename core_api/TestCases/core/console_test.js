dfdbf68652026245afc53db8009f0e63cf2839a2
delchev <delchevn@gmail.com>
2016-12-27 10:34:15 +0200

    tests moved to core_api_tests

--- core_api/TestCases/core/console_test.js
+++
@@ -1,8 +0,0 @@
-/* globals $ */
-/* eslint-env node, dirigible */
-
-console.info("Info message: %s", "Hello World!");
-console.error("Error message.");
-console.warn("Warning message.");
-console.log("Log message.");
-console.trace("Trace.");
